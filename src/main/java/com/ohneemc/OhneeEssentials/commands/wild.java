6c309c44b2cc62c57bb87574981b5eb96c137b62
Stian Tofte <stitofte@outlook.com>
2019-06-08 21:28:34 +0200

    First working state.

--- src/main/java/com/ohneemc/OhneeEssentials/commands/wild.java
+++ src/main/java/com/ohneemc/OhneeEssentials/commands/wild.java
@@ -2,9 +2,13 @@ package com.ohneemc.OhneeEssentials.commands;
 
 import com.ohneemc.OhneeEssentials.resources.intRandomizer;
 import com.ohneemc.OhneeEssentials.OhneeEssentials;
+import org.bukkit.Chunk;
 import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
 
 public class wild {
 
@@ -14,8 +18,9 @@ public class wild {
     public wild(OhneeEssentials plugin) {
         this.plugin = plugin;
     }
+    public wild (intRandomizer rand) {this.random = rand;}
 
-    public wild(Player player) {
+    public wild(Player player, Plugin plugin) {
         //FileConfiguration config = plugin.getConfig();
 
         //Getting world spawn
@@ -24,21 +29,38 @@ public class wild {
         int z = worldSpawn.getBlockZ();
 
         //Getting min and max from config
-        Integer maxX = plugin.config.getInt("WildTP.maxX");
-        Integer minX = plugin.config.getInt("WildTP.minX");
-        Integer maxZ = plugin.config.getInt("WildTP.maxZ");
-        Integer minZ = plugin.config.getInt("WildTP.minZ");
+        Integer maxX = plugin.getConfig().getInt("WildTP.maxX");
+        Integer minX = plugin.getConfig().getInt("WildTP.minX");
+        Integer maxZ = plugin.getConfig().getInt("WildTP.maxZ");
+        Integer minZ = plugin.getConfig().getInt("WildTP.minZ");
 
         //Ints for randomize - Originates from world spawn
-        int maxLX = x + maxX;
-        int minLX = x - minX;
-        int maxLZ = z + maxZ;
-        int minLZ = z - minZ;
+        int maxLX = x;
+        int minLX = x;
+        int maxLZ = z;
+        int minLZ = z;
+
+        maxLX = maxX + maxLX;
+        minLX = minX + minLX;
+        maxLZ = maxZ + maxLZ;
+        minLZ = minZ + minLZ;
 
         //Randomizing ints!
-        int rX = random.randomInt(maxLX, minLX);
-        int rZ = random.randomInt(maxLZ, minLZ);
+        final int rX = random.randomInt(maxLX, minLX);
+        final int rZ = random.randomInt(maxLZ, minLZ);
 
         player.sendMessage("Location: rX: " + rX + ", rZ: " + rZ);
+
+        Location toLoad = new Location(player.getWorld(), rX, 0 ,rZ);
+        Chunk destChunk = toLoad.getChunk();
+        //plugin.getLogger().info("Loading chunk at: x" + rX + ", z" + rZ);
+        destChunk.load();
+        //plugin.getLogger().info("Chunk loaded!");
+
+        Block hBlock = toLoad.getWorld().getHighestBlockAt(toLoad);
+
+        Location tp = new Location(toLoad.getWorld(), rX, hBlock.getY(), rZ);
+
+        player.teleport(tp);
     }
 }
