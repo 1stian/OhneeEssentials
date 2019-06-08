10538a1c0fa4888660e634cdbffc38c7bc8d3c97
Stian Tofte <stitofte@outlook.com>
2019-06-08 22:48:18 +0200

    Moved chunk loading and coordinate making to its own class.

--- src/main/java/com/ohneemc/OhneeEssentials/resources/chunkHelper.java
+++ src/main/java/com/ohneemc/OhneeEssentials/resources/chunkHelper.java
@@ -1,4 +1,27 @@
 package com.ohneemc.OhneeEssentials.resources;
 
+import org.bukkit.Chunk;
+import org.bukkit.Location;
+import org.bukkit.World;
+import org.bukkit.block.Block;
+
 public class chunkHelper {
+
+    public Location chunkLoader(Location chunkLoc){
+        Chunk chunk = chunkLoc.getChunk();
+        chunk.load();
+
+        //Getting highest block
+        Block hBlock = chunkLoc.getWorld().getHighestBlockAt(chunkLoc);
+
+        //Coordinates
+        World world = chunkLoc.getWorld();
+        int y = hBlock.getY();
+        int x = chunkLoc.getBlockX();
+        int z = chunkLoc.getBlockZ();
+
+
+        Location tp = new Location(world, x, y, z);
+        return tp;
+    }
 }
