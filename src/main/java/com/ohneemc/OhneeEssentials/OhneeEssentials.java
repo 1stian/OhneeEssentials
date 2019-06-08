75ffa43fea190d2474bb588a7ad4156136e24216
Stian Tofte <stian@toftesitservice.no>
2019-06-08 03:12:18 +0200

    Configs!!

--- src/main/java/com/ohneemc/OhneeEssentials/OhneeEssentials.java
+++ src/main/java/com/ohneemc/OhneeEssentials/OhneeEssentials.java
@@ -1,11 +1,17 @@
 package com.ohneemc.OhneeEssentials;
 
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class OhneeEssentials extends JavaPlugin {
 
+    public FileConfiguration config = this.getConfig();
+
     @Override
-    public void onEnable(){
+    public void onEnable() {
+        //Setting and getting config
+        config.options().copyDefaults(true);
+        this.saveConfig();
 
         //Register events class
         this.getServer().getPluginManager().registerEvents(new MyEvents(this), this);
@@ -14,7 +20,7 @@ public class OhneeEssentials extends JavaPlugin {
         this.getCommand("wild").setExecutor(new CommandClass(this));
     }
 
-    public void onDisable(){
+    public void onDisable() {
 
     }
 }
