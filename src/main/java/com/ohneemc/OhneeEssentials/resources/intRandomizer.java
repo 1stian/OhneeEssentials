a054f26412238031af0eb7fe3a047004ae722ece
Stian Tofte <stitofte@outlook.com>
2019-06-08 21:27:51 +0200

    Changed from random to Math.random.

--- src/main/java/com/ohneemc/OhneeEssentials/resources/intRandomizer.java
+++ src/main/java/com/ohneemc/OhneeEssentials/resources/intRandomizer.java
@@ -6,8 +6,12 @@ public class intRandomizer {
 
     public static int randomInt(int max, int min){
         //return random.nextInt(max - min + 1) + min;
-        Random random = new Random();
+        //Random random = new Random();
 
-        return random.ints(min, (max+1)).findFirst().getAsInt();
+        //return random.nextInt(max + 1 - min) + max;
+
+        int result = (int)(Math.random() * ((max -min) + 1 )) + min;
+
+        return result;
     }
 }
