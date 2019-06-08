3441ca70c1d43abc404d7749c1dfe3ca7ffa944e
Stian Tofte <stian@toftesitservice.no>
2019-06-08 03:11:28 +0200

    Getting started on the wild command

--- src/main/java/com/ohneemc/OhneeEssentials/CommandClass.java
+++ src/main/java/com/ohneemc/OhneeEssentials/CommandClass.java
@@ -1,18 +1,34 @@
 package com.ohneemc.OhneeEssentials;
 
+import com.ohneemc.OhneeEssentials.commands.wild;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public class CommandClass implements CommandExecutor {
 
     private OhneeEssentials plugin;
 
-    public CommandClass(OhneeEssentials plugin){
-        plugin = plugin;
+    public CommandClass(OhneeEssentials plugin) {
+        this.plugin = plugin;
     }
 
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        //Wild command
+        if (command.getName().equalsIgnoreCase("wild")) {
+            if (commandSender instanceof Player) {
+                if (commandSender.hasPermission("ohnee.wild")) {
+                    if (((Player) commandSender).getPlayer() != null){
+                        new wild(((Player) commandSender).getPlayer());
+                    }else{
+                        plugin.getLogger().warning("Player null... While using command /wild");
+                    }
+                    //new wild(((Player) commandSender).getPlayer());
+                    return true;
+                }
+            }
+        }
         return false;
     }
 }
