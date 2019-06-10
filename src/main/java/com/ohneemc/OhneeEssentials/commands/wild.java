76de845805da2b5dcbb93decec5bc6169362e2f9
Stian Tofte <stian@toftesitservice.no>
2019-06-10 22:14:47 +0200

    updated for ya

--- src/main/java/com/ohneemc/OhneeEssentials/commands/wild.java
+++ src/main/java/com/ohneemc/OhneeEssentials/commands/wild.java
@@ -3,11 +3,7 @@ package com.ohneemc.OhneeEssentials.commands;
 import com.ohneemc.OhneeEssentials.resources.chunkHelper;
 import com.ohneemc.OhneeEssentials.resources.intRandomizer;
 import com.ohneemc.OhneeEssentials.OhneeEssentials;
-import org.bukkit.Chunk;
 import org.bukkit.Location;
-import org.bukkit.World;
-import org.bukkit.block.Block;
-import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
@@ -24,35 +20,13 @@ public class wild {
     public wild(chunkHelper chunk) {this.chunk = chunk;}
 
     public wild(Player player, Plugin plugin) {
-        //FileConfiguration config = plugin.getConfig();
-
-        //Getting world spawn
-        Location worldSpawn = player.getWorld().getSpawnLocation();
-        int x = worldSpawn.getBlockX();
-        int z = worldSpawn.getBlockZ();
-
-        //Getting min and max from config
-        Integer maxX = plugin.getConfig().getInt("WildTP.maxX");
-        Integer minX = plugin.getConfig().getInt("WildTP.minX");
-        Integer maxZ = plugin.getConfig().getInt("WildTP.maxZ");
-        Integer minZ = plugin.getConfig().getInt("WildTP.minZ");
-
-        //Ints for randomize - Originates from world spawn
-        int maxLX = x;
-        int minLX = x;
-        int maxLZ = z;
-        int minLZ = z;
-
-        maxLX = maxX + maxLX;
-        minLX = minX + minLX;
-        maxLZ = maxZ + maxLZ;
-        minLZ = minZ + minLZ;
-
-        //Randomizing ints!
-        final int rX = random.randomInt(maxLX, minLX);
-        final int rZ = random.randomInt(maxLZ, minLZ);
-
-        Location toLoad = new Location(player.getWorld(), rX, 0 ,rZ);
-        player.teleport(chunk.chunkLoader(toLoad));
+        try {
+            System.out.println("chunk var debug: " + chunk);
+            Location tp = chunk.getLoc(player);
+            player.teleport(tp);
+        }catch (Exception ex){
+            player.sendMessage("Something went wrong.... Please tell your server admin!");
+            plugin.getLogger().warning(ex.toString());
+        }
     }
 }
