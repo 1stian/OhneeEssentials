4413168b1df75ce1e34fdf7de1dafc470c38d73e
Stian Tofte <stian@toftesitservice.no>
2019-06-14 01:56:13 +0200

    Added MessageHelper

--- src/main/java/com/ohneemc/OhneeEssentials/OhneeEssentials.java
+++ src/main/java/com/ohneemc/OhneeEssentials/OhneeEssentials.java
@@ -1,14 +1,21 @@
 package com.ohneemc.OhneeEssentials;
 
+import com.ohneemc.OhneeEssentials.commands.Ohnee;
 import com.ohneemc.OhneeEssentials.commands.Wild;
+import com.ohneemc.OhneeEssentials.resources.MessageHelper;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.HashMap;
 
 public class OhneeEssentials extends JavaPlugin {
 
     private FileConfiguration config = this.getConfig();
+    private OhneeEssentials pl = this;
 
-    private OhneeEssentials pl;
+    //Maps
+    public static final HashMap<Player, Long> coolmap = new HashMap<>();
 
     @SuppressWarnings("ConstantConditions")
     @Override
@@ -25,6 +32,10 @@ public class OhneeEssentials extends JavaPlugin {
 
         //Commands
         this.getCommand("Wild").setExecutor(new Wild(this));
+        this.getCommand("ohnee").setExecutor(new Ohnee(this));
+
+        //Create message file and load it
+        new MessageHelper(pl);
     }
 
     public void onDisable() {
