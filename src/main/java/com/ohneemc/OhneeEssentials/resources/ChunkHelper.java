05d789912a0b0236e4caa1ce4b0fb63f94c19f30
Stian Tofte <stian@toftesitservice.no>
2019-06-13 21:37:34 +0200

    Not being used for anything.. But later and is broken.

--- src/main/java/com/ohneemc/OhneeEssentials/resources/ChunkHelper.java
+++ src/main/java/com/ohneemc/OhneeEssentials/resources/ChunkHelper.java
@@ -32,25 +32,46 @@ public class ChunkHelper {
 
     private int retries = 0;
 
+    private boolean rerun = false;
+
     public ChunkHelper() {
 
     }
 
-    private  boolean chunkLoader(Location chunkLoc) {
+    private boolean chunkLoader(Location chunkLoc) {
         try {
-            Chunk chunk = chunkLoc.getChunk();
-            chunk.load();
-            return true;
+            //chunkLoc.getWorld().loadChunk(chunkLoc.getChunk().getX(), chunkLoc.getChunk().getZ(), true);
+            //chunkLoc.getWorld().getChunkAt(chunkLoc.getChunk().getX(), chunkLoc.getChunk().getZ());
+            //chunkLoc.getChunk().load(true);
+            System.out.println("Loading!");
+            return chunkLoc.getChunk().load(true);
         } catch (Exception ex) {
             plugin.getLogger().warning(ex.toString());
             return false;
         }
     }
 
-    private  boolean safeBlock(Material block) {
-        //Material block = loc.getBlock().getType();
-        //System.out.println("block var debug: " + block);
-        return !(block == Material.WATER || block == Material.LAVA);
+    private boolean isChunkLoded(Location loc) {
+        return loc.getWorld().isChunkLoaded(loc.getChunk().getX(), loc.getChunk().getZ());
+    }
+
+    private boolean safeBlock(Material block, Location check) {
+        int x = check.getBlockX();
+        int y = check.getBlockY();
+        int z = check.getBlockZ();
+
+        Location oneAbove = new Location(check.getWorld(), x, y + 1, z);
+        Location oneUnder = new Location(check.getWorld(), x, y - 1, z);
+
+        Material mY = check.getBlock().getType();
+        Material above = oneAbove.getBlock().getType();
+        Material under = oneAbove.getBlock().getType();
+
+        System.out.println("above var debug: " + above);
+        System.out.println("middle var debug: " + mY);
+        System.out.println("under var debug: " + under);
+
+        return (mY == Material.AIR || above == Material.AIR || under != Material.WATER && under != Material.LAVA);
     }
 
     public Location getLoc(Player player, Plugin plugin) {
@@ -70,36 +91,42 @@ public class ChunkHelper {
         maxLZ = z;
         minLZ = z;
 
-        //player.sendMessage("0");
-
         Location randomLoc = randomizer(worldLoc);
 
-        //player.sendMessage("1");
         if (chunkLoader(randomLoc)) {
-            //player.sendMessage("2");
-            if (safeBlock(randomLoc.getBlock().getType())) {
-                //player.sendMessage("3");
-                //Getting highest block
-                //int hBlock = randomLoc.getWorld().getHighestBlockYAt(randomLoc);
-                int tX = randomLoc.getBlockX();
-                int tY = randomLoc.getBlockY() + 1;
-                int tZ = randomLoc.getBlockX();
+            for (int i = 0; i < 30; i++) {
+                System.out.println("Number: " + i);
+                if (isChunkLoded(randomLoc)) {
+                    System.out.println("Oh it's loaded!");
+                    if (safeBlock(randomLoc.getBlock().getType(), randomLoc)) {
+                        System.out.println("And safe!! Woaah!");
+                        int tX = randomLoc.getBlockX();
+                        int tY = randomLoc.getBlockY();
+                        int tZ = randomLoc.getBlockX();
+                        //rerun = false;
 
-                return new Location(worldLoc, tX, tY, tZ);
-            } else {
-                if (retries < 5) {
-                    retries++;
-                    player.sendMessage("Trying to find location! Try: " + retries + "/5");
-                    getLoc(player, plugin);
-                } else {
-                    player.sendMessage("Couldn't find any safe locations.. Please try again.");
+                        return new Location(worldLoc, tX, tY, tZ);
+                    } else {
+                        if (retries < 5) {
+                            retries++;
+                            player.sendMessage("Trying to find location! Try: " + retries + "/5");
+                            getLoc(player, plugin);
+                        } else {
+                            player.sendMessage("Couldn't find any safe locations.. Please try again.");
+                        }
+                    }
+
+                    //i = 0;
+                }else{
+                    System.out.println("Not loaded!");
+                    i = 0;
                 }
             }
         }
         return null;
     }
 
-    private  Location randomizer(World world) {
+    private Location randomizer(World world) {
         maxLX = maxX + maxLX;
         minLX = minX + minLX;
         maxLZ = maxZ + maxLZ;
@@ -109,8 +136,16 @@ public class ChunkHelper {
         int rX = IntRandomizer.randomInt(maxLX, minLX);
         int rZ = IntRandomizer.randomInt(maxLZ, minLZ);
 
-        int y = world.getHighestBlockYAt(rX, rZ);
+        return getHighestBlock(world, rX, rZ);
+    }
 
-        return new Location(world, rX, y, rZ);
+    private Location getHighestBlock(World world, int x, int z) {
+        int i = 255;
+        while (i > 0) {
+            if (new Location(world, x, i, z).getBlock().getType() != Material.AIR)
+                return new Location(world, x, i, z).add(0, 1, 0);
+            i--;
+        }
+        return new Location(world, x, 1, z);
     }
 }
