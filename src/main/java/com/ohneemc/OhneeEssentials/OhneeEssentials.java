c717bc2d4d47277e22223a5fad1996b51325ef00
Stian Tofte <stian@toftesitservice.no>
2019-06-14 02:08:50 +0200

    Fixed a fault

--- src/main/java/com/ohneemc/OhneeEssentials/OhneeEssentials.java
+++ src/main/java/com/ohneemc/OhneeEssentials/OhneeEssentials.java
@@ -13,7 +13,6 @@ import java.util.HashMap;
 public class OhneeEssentials extends JavaPlugin {
 
     private FileConfiguration config = this.getConfig();
-    private OhneeEssentials pl = this;
 
     //Maps
     public static final HashMap<Player, Long> coolmap = new HashMap<>();
@@ -21,7 +20,6 @@ public class OhneeEssentials extends JavaPlugin {
     @SuppressWarnings("ConstantConditions")
     @Override
     public void onEnable() {
-        pl = this;
         //Setting and getting config
         config.options().copyDefaults(true);
         this.saveConfig();
@@ -29,14 +27,14 @@ public class OhneeEssentials extends JavaPlugin {
         this.reloadConfig();
 
         //Register events class
-        this.getServer().getPluginManager().registerEvents(new JoinQuitEvent(this), this);
+        this.getServer().getPluginManager().registerEvents(new JoinQuitEvent(), this);
 
         //Commands
         this.getCommand("Wild").setExecutor(new Wild(this));
         this.getCommand("ohnee").setExecutor(new Ohnee(this));
 
         //Create message file and load it
-        new MessageHelper(pl);
+        new MessageHelper(this);
     }
 
     public void onDisable() {
