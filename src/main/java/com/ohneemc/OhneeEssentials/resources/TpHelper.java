0a461c180d300d0cbeeef4f55c90f5efb66489a8
Stian Tofte <stian@toftesitservice.no>
2019-06-15 01:18:15 +0200

    Added teleport helper.

---
+++ src/main/java/com/ohneemc/OhneeEssentials/resources/TpHelper.java
@@ -0,0 +1,22 @@
+package com.ohneemc.OhneeEssentials.resources;
+
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+
+public class TpHelper {
+
+    public boolean Tp(Plugin plugin, String p1, String p2){
+        try{
+            Player one = plugin.getServer().getPlayer(p1);
+            Player two = plugin.getServer().getPlayer(p2);
+            if (one != null && two != null){
+                one.teleport(two.getLocation());
+                return true;
+            }
+            return false;
+        }catch (Exception ex){
+            ex.printStackTrace();
+        }
+        return false;
+    }
+}
