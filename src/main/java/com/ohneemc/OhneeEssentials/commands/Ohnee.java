973edaefbdfb459ee215386aceffac3cffc2ac2c
Stian Tofte <stian@toftesitservice.no>
2019-06-14 02:09:27 +0200

    Starting admin command! Reload etc.

---
+++ src/main/java/com/ohneemc/OhneeEssentials/commands/Ohnee.java
@@ -0,0 +1,17 @@
+package com.ohneemc.OhneeEssentials.commands;
+
+import com.ohneemc.OhneeEssentials.OhneeEssentials;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class Ohnee implements CommandExecutor {
+    public Ohnee(OhneeEssentials ohneeEssentials) {
+    }
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+
+        return false;
+    }
+}
