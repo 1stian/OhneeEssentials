46bf92659172ff67d336dfd26a04cbcbad83b1a4
Stian Tofte <stian@toftesitservice.no>
2019-06-14 21:30:58 +0200

    Added reload and version commands.

--- src/main/java/com/ohneemc/OhneeEssentials/commands/Ohnee.java
+++ src/main/java/com/ohneemc/OhneeEssentials/commands/Ohnee.java
@@ -4,14 +4,46 @@ import com.ohneemc.OhneeEssentials.OhneeEssentials;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.Arrays;
 
 public class Ohnee implements CommandExecutor {
+    private OhneeEssentials plugin;
     public Ohnee(OhneeEssentials ohneeEssentials) {
+        this.plugin = ohneeEssentials;
     }
 
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        if (command.getName().equalsIgnoreCase("Ohnee")) {
+            if (((Player) commandSender).getPlayer() != null) {
+                Player player = ((Player) commandSender).getPlayer();
+                if (!(strings.length < 1)){
+                    System.out.print(strings[0]);
+                    switch (strings[0]){
+                        case "reload":
+                            plugin.getConfig();
+                            if (player != null) player.sendMessage("Config has been reloaded");
+                            break;
 
+                        case "version":
+                            String ver = plugin.getDescription().getVersion();
+                            if (player != null) player.sendMessage("OhneeEssentials version: " + ver);
+                            break;
+                    }
+
+                    return true;
+
+                }
+
+
+            } else {
+                plugin.getLogger().warning("Player null... While using command /Wild");
+                return false;
+            }
+            //new Wild(((Player) commandSender).getPlayer());
+        }
         return false;
     }
 }
