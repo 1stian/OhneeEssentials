c66cb13c9353d0a46efb922e4776a531f0e8aac1
Stian Tofte <stian@toftesitservice.no>
2019-06-16 03:24:35 +0200

    Tphere command added with helper.

--- src/main/java/com/ohneemc/OhneeEssentials/commands/Tphere.java
+++ src/main/java/com/ohneemc/OhneeEssentials/commands/Tphere.java
@@ -1,4 +1,26 @@
 package com.ohneemc.OhneeEssentials.commands;
 
-public class Tphere {
+import com.ohneemc.OhneeEssentials.OhneeEssentials;
+import com.ohneemc.OhneeEssentials.resources.TpHelper;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Tphere implements CommandExecutor {
+    private OhneeEssentials plugin;
+    public Tphere(OhneeEssentials ohneeEssentials) {
+        this.plugin = ohneeEssentials;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        if (command.getName().equalsIgnoreCase("Tphere") && commandSender instanceof Player){
+            if (!(strings.length < 1)) {
+                return new TpHelper().Tp(plugin, strings[0], commandSender.getName());
+            }
+            return false;
+        }
+        return false;
+    }
 }
