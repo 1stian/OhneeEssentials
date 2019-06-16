7114bf444d3a2187d55d6ad296cb198296b82886
Stian Tofte <stian@toftesitservice.no>
2019-06-16 03:37:36 +0200

    Added time command.

--- src/main/java/com/ohneemc/OhneeEssentials/commands/Time.java
+++ src/main/java/com/ohneemc/OhneeEssentials/commands/Time.java
@@ -1,4 +1,38 @@
 package com.ohneemc.OhneeEssentials.commands;
 
-public class Time {
+import com.ohneemc.OhneeEssentials.OhneeEssentials;
+import org.bukkit.World;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Time implements CommandExecutor {
+    private  OhneeEssentials plugin;
+    public Time(OhneeEssentials ohneeEssentials) {
+        this.plugin = ohneeEssentials;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        if (command.getName().equalsIgnoreCase("Time") && commandSender instanceof Player){
+            Player player = ((Player) commandSender).getPlayer();
+            World world = player.getWorld();
+
+            String type = strings[0];
+
+            switch (type){
+                case "day":
+                    world.setTime(8000);
+                    player.sendMessage("Time set to day!");
+                    return true;
+                case "night":
+                    world.setTime(20000);
+                    player.sendMessage("Time set to night!");
+                    return true;
+            }
+            return false;
+        }
+        return false;
+    }
 }
