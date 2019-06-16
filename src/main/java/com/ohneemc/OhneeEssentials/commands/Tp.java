6e7f3730ad89043320fae0dac1abcb16d3ffae0e
Stian Tofte <stian@toftesitservice.no>
2019-06-16 03:24:16 +0200

    Tp command added with helper.

--- src/main/java/com/ohneemc/OhneeEssentials/commands/Tp.java
+++ src/main/java/com/ohneemc/OhneeEssentials/commands/Tp.java
@@ -1,4 +1,26 @@
 package com.ohneemc.OhneeEssentials.commands;
 
-public class Tp {
+import com.ohneemc.OhneeEssentials.OhneeEssentials;
+import com.ohneemc.OhneeEssentials.resources.TpHelper;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Tp implements CommandExecutor {
+    private OhneeEssentials plugin;
+    public Tp(OhneeEssentials ohneeEssentials) {
+        this.plugin = ohneeEssentials;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        if (command.getName().equalsIgnoreCase("Tp") && commandSender instanceof Player){
+            if (!(strings.length < 1)) {
+                return new TpHelper().Tp(plugin, commandSender.getName(), strings[0]);
+            }
+            return false;
+        }
+        return false;
+    }
 }
