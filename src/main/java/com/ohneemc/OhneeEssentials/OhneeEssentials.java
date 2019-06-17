1c57c9f210aa76b9cb9d7b98a0227eb2686fd7d7
Stian Tofte <stian@toftesitservice.no>
2019-06-17 20:00:49 +0200

    Testing jenkins trigger!

--- src/main/java/com/ohneemc/OhneeEssentials/OhneeEssentials.java
+++ src/main/java/com/ohneemc/OhneeEssentials/OhneeEssentials.java
@@ -67,7 +67,7 @@ public class OhneeEssentials extends JavaPlugin {
         this.getCommand("Setwarp").setExecutor(new Setwarp(this));
         this.getCommand("Delwarp").setExecutor(new Delwarp(this));
 
-        //Testing jenkins1
+        //Testing jenkins2
     }
 
     public void onDisable() {
