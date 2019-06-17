7cbd17f892f3a80a6e3d70accf1cf350ef73bb5b
Stian Tofte <stian@toftesitservice.no>
2019-06-17 02:04:32 +0200

    Warp command.

--- src/main/java/com/ohneemc/OhneeEssentials/commands/Warp.java
+++ src/main/java/com/ohneemc/OhneeEssentials/commands/Warp.java
@@ -1,4 +1,37 @@
 package com.ohneemc.OhneeEssentials.commands;
 
-public class Warp {
+import com.google.common.base.Strings;
+import com.ohneemc.OhneeEssentials.OhneeEssentials;
+import org.bukkit.Location;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import sun.rmi.runtime.Log;
+
+public class Warp implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        if (command.getName().equalsIgnoreCase("Warp") && commandSender instanceof Player) {
+            Player player = ((Player) commandSender).getPlayer();
+            String warpName = null;
+
+            if (strings.length < 1) {
+                return false;
+            } else {
+                warpName = strings[0].toLowerCase();
+            }
+
+            if (!OhneeEssentials.warpMap.containsKey(warpName)) {
+                player.sendMessage("A warp with the name: " + warpName + " does not exist!");
+                return true;
+            } else {
+                Location loc = OhneeEssentials.warpMap.get(warpName);
+                player.teleport(loc);
+                player.sendMessage("You've been teleported to: " + warpName);
+                return true;
+            }
+        }
+        return false;
+    }
 }
