002131e9abc3ad8931df3824f1d00bad584423bb
Stian Tofte <stian@toftesitservice.no>
2019-06-18 23:25:36 +0200

    Added userdata files(Not tested)

--- src/main/java/com/ohneemc/OhneeEssentials/events/JoinQuitEvent.java
+++ src/main/java/com/ohneemc/OhneeEssentials/events/JoinQuitEvent.java
@@ -1,10 +1,18 @@
 package com.ohneemc.OhneeEssentials.events;
 
 import com.ohneemc.OhneeEssentials.OhneeEssentials;
+import com.ohneemc.OhneeEssentials.commands.Time;
+import de.leonhard.storage.Json;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class JoinQuitEvent implements Listener {
 
@@ -12,6 +20,8 @@ public class JoinQuitEvent implements Listener {
     public JoinQuitEvent(OhneeEssentials plugin) {
         this.plugin = plugin;
     }
+
+    Json userdata;
 
     public JoinQuitEvent() {
 
@@ -22,10 +32,48 @@ public class JoinQuitEvent implements Listener {
         if (!plugin.coolmap.containsKey(e.getPlayer())){
             plugin.coolmap.put(e.getPlayer(), (System.currentTimeMillis() / 1000));
         }
+
+        File uData = new File("userdata");
+        if(!uData.exists()){
+            try{
+                uData.mkdir();
+            }catch (Exception ex){
+                ex.printStackTrace();
+            }
+        }
+
+        File first = new File(plugin.getDataFolder() + "userdata/" + e.getPlayer().getUniqueId().toString());
+        if (!first.exists()){
+            firstJoin(e.getPlayer());
+            userdata = new Json(e.getPlayer().getUniqueId().toString(), plugin.getDataFolder().toString() + "userdata/");
+        }else{
+            SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+            Date date = new Date(System.currentTimeMillis());
+
+            userdata = new Json(e.getPlayer().getUniqueId().toString(), plugin.getDataFolder().toString() + "userdata/");
+            userdata.set("PlayerInfo.Name", e.getPlayer().getName());
+            userdata.set("PlayerInfo.UUID", e.getPlayer().getUniqueId().toString());
+            userdata.set("PlayerStats.lastSessionStarted", formatter.format(date));
+        }
     }
 
     @EventHandler
     public void onQuit(PlayerQuitEvent e) {
+
+    }
+
+    public void firstJoin(Player player){
+        SimpleDateFormat formatter= new SimpleDateFormat("yyyy-MM-dd 'at' HH:mm:ss z");
+        Date date = new Date(System.currentTimeMillis());
+
+        userdata.set("PlayerInfo.Name", player.getPlayer().getName());
+        userdata.set("PlayerInfo.UUID", player.getPlayer().getUniqueId().toString());
+        userdata.set("PlayerStats.FirstSeen", formatter.format(date));
+        userdata.set("PlayerStats.Playtime", 0);
+        userdata.set("PlayerStats.lastSessionStarted", formatter.format(date));
+    }
+
+    public void userLeave(Player player){
 
     }
 }
