4555dc187f063a706ae4b3563d031ed24579780c
Stian Tofte <stian@toftesitservice.no>
2019-06-22 12:51:52 +0200

    Warp loading updated to new storage.(JSON,TOML,YAML). No mysql yet.

--- src/main/java/com/ohneemc/OhneeEssentials/resources/WarpConfigHelper.java
+++ src/main/java/com/ohneemc/OhneeEssentials/resources/WarpConfigHelper.java
@@ -1,13 +1,10 @@
 package com.ohneemc.OhneeEssentials.resources;
 
-import com.google.common.base.Strings;
 import com.ohneemc.OhneeEssentials.OhneeEssentials;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.World;
-import org.bukkit.configuration.InvalidConfigurationException;
 
-import java.io.IOException;
 import java.util.Set;
 
 public class WarpConfigHelper {
@@ -31,37 +28,88 @@ public class WarpConfigHelper {
 
     public void warpLoad() {
         try {
-            plugin.warpConfig().load(plugin.getWarpFile());
 
-            Set<String> names = plugin.warpConfig().getKeys(false);
-            if (!(names.isEmpty())){
-                for (String warp : names) {
-                    String n1 = warp.replaceAll("\\[", "");
-                    String name = n1.replaceAll("\\]", "");
-                    World world = plugin.getServer().getWorld(plugin.warpConfig().getString(name + ".world"));
-                    double x = plugin.warpConfig().getDouble(name + ".x");
-                    double y = plugin.warpConfig().getDouble(name + ".y");
-                    double z = plugin.warpConfig().getDouble(name + ".z");
-                    float pitch = plugin.warpConfig().getLong(name + ".pitch");
-                    float yaw = plugin.warpConfig().getLong(name + ".yaw");
-
-                    Location loc = new Location(world, x, y, z, pitch, yaw);
-                    OhneeEssentials.warpMap.put(warp, loc);
-                }
+            if (plugin.settings().getBoolean("PluginSettings.Warp.json")){
+                loadJson();
+            }else if (plugin.settings().getBoolean("PluginSettings.Warp.toml")){
+                loadToml();
+            }else if (plugin.settings().getBoolean("PluginSettings.Warp.yaml")){
+                loadYaml();
             }
-        } catch (IOException e) {
-            e.printStackTrace();
-        } catch (InvalidConfigurationException e) {
+
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void saveFile() {
-        try {
-            plugin.warpConfig().save(plugin.getWarpFile());
-        } catch (IOException ex) {
-            ex.printStackTrace();
+    private void loadJson(){
+        Set<String> names = plugin.jsonWarps().getKeySet();
+
+        if (!(names.isEmpty())){
+            for (String warp : names) {
+                String n1 = warp.replaceAll("\\[", "");
+                String name = n1.replaceAll("\\]", "");
+                World world = plugin.getServer().getWorld(plugin.jsonWarps().getString(name + ".world"));
+                double x = plugin.jsonWarps().getDouble(name + ".x");
+                double y = plugin.jsonWarps().getDouble(name + ".y");
+                double z = plugin.jsonWarps().getDouble(name + ".z");
+                Float pitch = plugin.jsonWarps().getFloat(name + ".pitch");
+                Float yaw = plugin.jsonWarps().getFloat(name + ".yaw");
+
+                Location loc = new Location(world, x, y, z, yaw, pitch);
+                OhneeEssentials.warpMap.put(warp, loc);
+            }
         }
+
+        plugin.getLogger().info("Warps loaded: " + names.toString());
+    }
+
+    private void loadToml(){
+        Set<String> names = plugin.tomlWarps().getKeySet();
+
+        if (!(names.isEmpty())){
+            for (String warp : names) {
+                String n1 = warp.replaceAll("\\[", "");
+                String name = n1.replaceAll("\\]", "");
+                World world = plugin.getServer().getWorld(plugin.tomlWarps().getString(name + ".world"));
+                double x = plugin.tomlWarps().getDouble(name + ".x");
+                double y = plugin.tomlWarps().getDouble(name + ".y");
+                double z = plugin.tomlWarps().getDouble(name + ".z");
+                Float pitch = plugin.tomlWarps().getFloat(name + ".pitch");
+                Float yaw = plugin.tomlWarps().getFloat(name + ".yaw");
+
+                Location loc = new Location(world, x, y, z, yaw, pitch);
+                OhneeEssentials.warpMap.put(warp, loc);
+            }
+        }
+
+        plugin.getLogger().info("Warps loaded: " + names.toString());
+    }
+
+    private void loadYaml(){
+        Set<String> names = plugin.yamlWarps().getKeySet();
+
+        if (!(names.isEmpty())){
+            for (String warp : names) {
+                String n1 = warp.replaceAll("\\[", "");
+                String name = n1.replaceAll("\\]", "");
+                World world = plugin.getServer().getWorld(plugin.yamlWarps().getString(name + ".world"));
+                double x = plugin.yamlWarps().getDouble(name + ".x");
+                double y = plugin.yamlWarps().getDouble(name + ".y");
+                double z = plugin.yamlWarps().getDouble(name + ".z");
+                Float pitch = plugin.yamlWarps().getFloat(name + ".pitch");
+                Float yaw = plugin.yamlWarps().getFloat(name + ".yaw");
+
+                Location loc = new Location(world, x, y, z, yaw, pitch);
+                OhneeEssentials.warpMap.put(warp, loc);
+            }
+        }
+
+        plugin.getLogger().info("Warps loaded: " + names.toString());
+    }
+
+    public void saveFile() {
+        //plugin.jsonWarps().update();
     }
 
     public void saveMySql() {
