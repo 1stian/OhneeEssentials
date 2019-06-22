e37a3114cf76f41edd8d7ef349c0ee16f7597795
Stian Tofte <stian@toftesitservice.no>
2019-06-22 21:15:15 +0200

    Added tpa,tpahere,tpaccept,tpdeny and maps class.

--- src/main/java/com/ohneemc/OhneeEssentials/commands/Tpa.java
+++ src/main/java/com/ohneemc/OhneeEssentials/commands/Tpa.java
@@ -1,4 +1,49 @@
 package com.ohneemc.OhneeEssentials.commands;
 
-public class Tpa {
+import com.ohneemc.OhneeEssentials.OhneeEssentials;
+import com.ohneemc.OhneeEssentials.resources.Maps;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Tpa  implements CommandExecutor {
+    private OhneeEssentials ohnee;
+    public Tpa (OhneeEssentials ohnee){
+        this.ohnee = ohnee;
+    }
+
+    private Maps maps;
+    public Tpa(Maps maps){
+        this.maps = maps;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("Tpa") && sender instanceof Player){
+            Player player = ((Player) sender).getPlayer();
+            Player target;
+
+            if (args.length < 1){
+                return false;
+            }else if (ohnee.getServer().getPlayer(args[0].toLowerCase()) != null){
+                target = ohnee.getServer().getPlayer(args[0]);
+                if (!maps.tp().containsKey(target)){
+                    String toMap = player.getName() + "," + System.currentTimeMillis();
+                    maps.tp().put(target, toMap);
+                    player.sendMessage("Teleport request sent!");
+                    target.sendMessage(player.getName() + " has sent you a teleport request - /tpaaccept or /tpadeny");
+                    return true;
+                }else{
+                    player.sendMessage(target.getName() + " already has a pending request.");
+                    return true;
+                }
+            }else{
+                player.sendMessage("Couldn't find player");
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
--- src/main/java/com/ohneemc/OhneeEssentials/commands/Tpdeny.java
+++ src/main/java/com/ohneemc/OhneeEssentials/commands/Tpdeny.java
@@ -1,4 +1,36 @@
 package com.ohneemc.OhneeEssentials.commands;
 
-public class Tpdeny {
+import com.ohneemc.OhneeEssentials.OhneeEssentials;
+import com.ohneemc.OhneeEssentials.resources.Maps;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class Tpdeny implements CommandExecutor {
+    private OhneeEssentials ohnee;
+    public Tpdeny (OhneeEssentials ohnee){
+        this.ohnee = ohnee;
+    }
+
+    private Maps maps;
+    public Tpdeny(Maps maps){
+        this.maps = maps;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (command.getName().equalsIgnoreCase("Tpdeny") && sender instanceof Player){
+            Player player = ((Player) sender).getPlayer();
+            String extract = maps.tp().get(player.getName()).toString();
+            String[] split = extract.split(",");
+
+            Player target = ohnee.getServer().getPlayer(split[0]);
+
+            maps.tp().remove(player);
+            target.sendMessage(player.getName() + " denied your request.");
+            return true;
+        }
+        return false;
+    }
 }
