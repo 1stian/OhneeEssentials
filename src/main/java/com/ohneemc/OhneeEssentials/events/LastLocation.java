9ca36dc266319cd1307c0188babb8cdbdde0a4cd
Stian Tofte <stian@toftesitservice.no>
2019-06-22 18:37:44 +0200

    Added back command, with a teleport listener

---
+++ src/main/java/com/ohneemc/OhneeEssentials/events/LastLocation.java
@@ -0,0 +1,24 @@
+package com.ohneemc.OhneeEssentials.events;
+
+import com.ohneemc.OhneeEssentials.OhneeEssentials;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerTeleportEvent;
+
+public class LastLocation implements Listener {
+
+    private OhneeEssentials ohnee;
+    public LastLocation(OhneeEssentials ohnee){
+        this.ohnee = ohnee;
+    }
+
+    @EventHandler
+    public void lastLoc(PlayerTeleportEvent e){
+        Player player = e.getPlayer();
+        Location loc = player.getLocation();
+
+        ohnee.lLoc().put(player.getUniqueId(), loc);
+    }
+}
