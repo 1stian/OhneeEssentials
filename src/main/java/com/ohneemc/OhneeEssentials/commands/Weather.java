d7dc06b9f55ac1c0ed49b805c9e6b1239b5367a6
Stian Tofte <stian@toftesitservice.no>
2019-06-23 20:58:21 +0200

    Cleaned up the code a bit.

--- src/main/java/com/ohneemc/OhneeEssentials/commands/Weather.java
+++ src/main/java/com/ohneemc/OhneeEssentials/commands/Weather.java
@@ -1,6 +1,5 @@
 package com.ohneemc.OhneeEssentials.commands;
 
-import com.ohneemc.OhneeEssentials.OhneeEssentials;
 import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -8,16 +7,11 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public class Weather implements CommandExecutor {
-    private OhneeEssentials plugin;
-    public Weather(OhneeEssentials ohneeEssentials) {
-        this.plugin = ohneeEssentials;
-    }
-
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
         if (command.getName().equalsIgnoreCase("Weather") && commandSender instanceof Player) {
             Player player = ((Player) commandSender).getPlayer();
-            World world = player.getWorld();
+            World world = player != null ? player.getWorld() : null;
 
             if (strings.length < 1){
                 return false;
@@ -32,25 +26,32 @@ public class Weather implements CommandExecutor {
 
             switch (type){
                 case "clear":
-                    world.setThundering(false);
-                    world.setStorm(false);
-                    world.setWeatherDuration(0);
-                    player.sendMessage("Weather has been set to clear!");
-                    return true;
+                    if (world != null) {
+                        world.setThundering(false);
+                        world.setStorm(false);
+                        world.setWeatherDuration(0);
+                        player.sendMessage("Weather has been set to clear!");
+                        return true;
+                    }
                 case "thunder":
-                    world.setThundering(true);
-                    world.setWeatherDuration(6000);
-                    player.sendMessage("Is that... Thunder?!");
-                    return true;
-                case "rain":
-                    if (duration != null){
-                        int time = Integer.parseInt(duration.toLowerCase());
-                        world.setWeatherDuration(time * 20);
-                        player.sendMessage("Weather has been set to rain");
-                    }else{
-                        world.setStorm(true);
+                    if (world != null) {
+                        world.setThundering(true);
                         world.setWeatherDuration(6000);
-                        player.sendMessage("Weather has been set to rain");
+                        player.sendMessage("Is that... Thunder?!");
+                        return true;
+                    }
+                case "rain":
+                    if (world != null) {
+                        if (duration != null){
+                            int time = Integer.parseInt(duration.toLowerCase());
+                            world.setWeatherDuration(time * 20);
+                            player.sendMessage("Weather has been set to rain");
+                        }else{
+                            world.setStorm(true);
+                            world.setWeatherDuration(6000);
+                            player.sendMessage("Weather has been set to rain");
+                        }
+                        return true;
                     }
                     return true;
             }
