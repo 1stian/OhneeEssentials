2d7dff59a7b4f63091205de8725f6a891285198a
Stian Tofte <stitofte@outlook.com>
2019-09-09 17:56:08 +0200

    Disabled import command.

--- src/main/java/com/ohneemc/OhneeEssentials/OhneeEssentials.java
+++ src/main/java/com/ohneemc/OhneeEssentials/OhneeEssentials.java
@@ -183,7 +183,7 @@ public class OhneeEssentials extends JavaPlugin {
         this.getCommand("sethome").setExecutor(new Sethome(this));
         this.getCommand("home").setExecutor(new Home(this));
         this.getCommand("delhome").setExecutor(new Delhome(this));
-        this.getCommand("import").setExecutor(new Import(this));
+        //this.getCommand("import").setExecutor(new Import(this));
     }
 
     private void registerEvents() {
