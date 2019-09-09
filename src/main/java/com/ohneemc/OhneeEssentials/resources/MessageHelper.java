8011f071371135be5e95e8738808d09a9a6b420f
Stian Tofte <stitofte@outlook.com>
2019-09-09 23:52:34 +0200

    Removed code that was moved to a different class.

--- src/main/java/com/ohneemc/OhneeEssentials/resources/MessageHelper.java
+++ src/main/java/com/ohneemc/OhneeEssentials/resources/MessageHelper.java
@@ -68,13 +68,4 @@ public class MessageHelper {
             e.printStackTrace();
         }
     }
-
-    public String messageCreater(String[] message){
-        StringBuilder sb = new StringBuilder();
-        for(int i = 0; i < message.length; i++) {
-            if (i > 0) sb.append(" ");
-            sb.append(message[i]);
-        }
-        return sb.toString();
-    }
 }
