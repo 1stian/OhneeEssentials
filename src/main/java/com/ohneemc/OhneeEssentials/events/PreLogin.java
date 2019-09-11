34ef7e6bd0f094e9d188245afe4ade16ef254380
Stian Tofte <stitofte@outlook.com>
2019-09-12 00:46:44 +0200

    Prelogin check for scoreboard(Anti flicker)

---
+++ src/main/java/com/ohneemc/OhneeEssentials/events/PreLogin.java
@@ -0,0 +1,26 @@
+package com.ohneemc.OhneeEssentials.events;
+
+import com.ohneemc.OhneeEssentials.OhneeEssentials;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
+import org.bukkit.scoreboard.Score;
+
+public class PreLogin implements Listener {
+    private OhneeEssentials plugin;
+    public PreLogin(OhneeEssentials plugin){this.plugin = plugin;}
+
+    @EventHandler
+    public void LoginListen(AsyncPlayerPreLoginEvent e){
+        Score score = plugin.o.getScore("" + Bukkit.getOnlinePlayers().size());
+        int previousScore = score.getScore();
+        plugin.setSb().put(e.getUniqueId(), true);
+
+        plugin.s.resetScores("" + Bukkit.getOnlinePlayers().size());
+
+        score = plugin.o.getScore("" + Bukkit.getOnlinePlayers().size() + 1);
+        score.setScore(previousScore);
+    }
+}
