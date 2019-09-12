6b35cfb186da908c5a868cd104d5107853244bf9
Stian Tofte <stitofte@outlook.com>
2019-09-12 22:17:30 +0200

    Added tp player target.

--- src/main/java/com/ohneemc/OhneeEssentials/commands/Tp.java
+++ src/main/java/com/ohneemc/OhneeEssentials/commands/Tp.java
@@ -2,6 +2,7 @@ package com.ohneemc.OhneeEssentials.commands;
 
 import com.ohneemc.OhneeEssentials.OhneeEssentials;
 import com.ohneemc.OhneeEssentials.resources.TpHelper;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -16,8 +17,19 @@ public class Tp implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
         if (command.getName().equalsIgnoreCase("Tp") && commandSender instanceof Player){
+            if (strings.length == 2){
+                if (new TpHelper().Tp(plugin, strings[0], strings[1])){
+                    commandSender.sendMessage(ChatColor.GREEN + "Players have been teleported.");
+                    return true;
+                }else{
+                    commandSender.sendMessage(ChatColor.GREEN + "Couldn't find players specified.");
+                }
+            }
+
             if (!(strings.length < 1)) {
-                return new TpHelper().Tp(plugin, commandSender.getName(), strings[0]);
+                if (new TpHelper().Tp(plugin, commandSender.getName(), strings[0])){
+                    return true;
+                }
             }
             return false;
         }
