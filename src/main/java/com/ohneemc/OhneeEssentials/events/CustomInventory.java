ce104abc4ab69ccc0674045a9ebd8c750d3046de
Stian Tofte <stitofte@outlook.com>
2019-09-13 22:12:26 +0200

    Custom inventory for wild command added.

---
+++ src/main/java/com/ohneemc/OhneeEssentials/events/CustomInventory.java
@@ -0,0 +1,61 @@
+package com.ohneemc.OhneeEssentials.events;
+
+import com.ohneemc.OhneeEssentials.OhneeEssentials;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Listener;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.plugin.Plugin;
+
+public class CustomInventory implements Listener {
+    private Plugin plugin = OhneeEssentials.getPlugin(OhneeEssentials.class);
+
+    public void newWildInventory(Player player){
+        Inventory i = plugin.getServer().createInventory(null, 27, ChatColor.GREEN + "WildTP");
+        ItemStack empty = new ItemStack(Material.BLACK_STAINED_GLASS_PANE, 1);
+        ItemMeta emptyMeta = empty.getItemMeta();
+        emptyMeta.setDisplayName(" ");
+        empty.setItemMeta(emptyMeta);
+
+        ItemStack overworld = new ItemStack(Material.SUNFLOWER, 1);
+        ItemMeta oMeta = overworld.getItemMeta();
+        oMeta.setDisplayName(ChatColor.YELLOW + "Teleport to a random location in the overworld.");
+        overworld.setItemMeta(oMeta);
+        ItemStack nether = new ItemStack(Material.NETHER_STAR, 1);
+        ItemMeta nMeta = overworld.getItemMeta();
+        nMeta.setDisplayName(ChatColor.RED + "Teleport to a random location in the nether.");
+        nether.setItemMeta(nMeta);
+
+        i.setItem(0,empty);
+        i.setItem(1,empty);
+        i.setItem(2,empty);
+        i.setItem(3,empty);
+        i.setItem(4,empty);
+        i.setItem(5,empty);
+        i.setItem(6,empty);
+        i.setItem(7,empty);
+        i.setItem(8,empty);
+        i.setItem(9,empty);
+        i.setItem(10,empty);
+        i.setItem(11,empty);
+        i.setItem(12, overworld);
+        i.setItem(13,empty);
+        i.setItem(14, nether);
+        i.setItem(15,empty);
+        i.setItem(16,empty);
+        i.setItem(17,empty);
+        i.setItem(18,empty);
+        i.setItem(19,empty);
+        i.setItem(20,empty);
+        i.setItem(21,empty);
+        i.setItem(22,empty);
+        i.setItem(23,empty);
+        i.setItem(24,empty);
+        i.setItem(25,empty);
+        i.setItem(26,empty);
+        player.openInventory(i);
+    }
+}
