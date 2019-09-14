04f01d54dc774f65567455e22c5ef6605d623e38
Stian Tofte <stitofte@outlook.com>
2019-09-14 17:54:36 +0200

    Updated to support two worlds. overworld and nether

--- src/main/java/com/ohneemc/OhneeEssentials/commands/Wild.java
+++ src/main/java/com/ohneemc/OhneeEssentials/commands/Wild.java
@@ -10,13 +10,11 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
-import org.bukkit.event.inventory.ClickType;
 import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.InventoryView;
 import org.bukkit.inventory.ItemStack;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -26,23 +24,31 @@ public class Wild implements CommandExecutor, Listener {
 
 
     //Settings
-    private int maxX;
-    private int minX;
-    private int maxZ;
-    private int minZ;
+    private int w1MAxX;
+    private int w1MinX;
+    private int w1MaxZ;
+    private int w1MinZ;
+    private int w2MAxX;
+    private int w2MinX;
+    private int w2MaxZ;
+    private int w2MinZ;
     private int cooldown;
 
     private boolean running = false;
-    private List<Material> materials = new ArrayList<>();
+    private List<Material> materials;
     private boolean hitNogo = false;
 
     public Wild(OhneeEssentials plugin) {
         this.plugin = plugin;
         this.cooldown = plugin.cooldown;
-        this.maxX = plugin.maxX;
-        this.minX = plugin.minX;
-        this.maxZ = plugin.maxZ;
-        this.minZ = plugin.minZ;
+        this.w1MAxX = plugin.w1MaxX;
+        this.w1MinX = plugin.w1MinX;
+        this.w1MaxZ = plugin.w1MaxZ;
+        this.w1MinZ = plugin.w1Minz;
+        this.w2MAxX = plugin.w2MaxX;
+        this.w2MinX = plugin.w2MinX;
+        this.w2MaxZ = plugin.w2MaxZ;
+        this.w2MinZ = plugin.w2Minz;
         this.materials = plugin.materials;
     }
 
@@ -57,19 +63,10 @@ public class Wild implements CommandExecutor, Listener {
                 if (!running) {
 
                     CustomInventory i = new CustomInventory();
-                    i.newWildInventory(player);
-                    return true;
-                    //Old under here! Comment out when I am done!
-                    /**
-                    running = true;
-                    sender.sendMessage(ChatColor.GREEN + "Looking for a safe location. Hold on.");
-                    String world = null;
-                    if (strings.length == 1) {
-                        world = strings[0];
+                    if (player != null) {
+                        i.newWildInventory(player);
                     }
-
-                    return runWild(player, world);
-                     **/
+                    return true;
                 } else {
                     sender.sendMessage(ChatColor.GREEN + "You're already being teleported!");
                     return true;
@@ -80,100 +77,103 @@ public class Wild implements CommandExecutor, Listener {
         return false;
     }
 
+    @SuppressWarnings("ConstantConditions")
     @EventHandler
-    public void onClick(InventoryClickEvent e){
+    public void onClick(InventoryClickEvent e) {
         Player player = (Player) e.getWhoClicked();
-        ClickType click = e.getClick();
+        //ClickType click = e.getClick();
         Inventory open = e.getClickedInventory();
         InventoryView title = e.getView();
         String invTitle = title.getTitle();
         ItemStack item = e.getCurrentItem();
 
-        if (open == null){
+        if (open == null) {
             return;
         }
-        if (invTitle.equalsIgnoreCase(ChatColor.DARK_GREEN + "WildTP - Chose world below.")){
-            if (item.equals(null) || !item.hasItemMeta()){
+        if (invTitle.equalsIgnoreCase(ChatColor.DARK_GREEN + "WildTP - Chose world below.")) {
+            if (item == null || !item.hasItemMeta()) {
                 return;
             }
-            if (item.getItemMeta().getDisplayName().equals(ChatColor.YELLOW + "Teleport to a random location in the overworld.")){
+
+            if (item.getItemMeta().getDisplayName().equals(ChatColor.YELLOW + "Teleport to a random location in the overworld.")) {
                 player.closeInventory();
                 e.getWhoClicked().sendMessage(ChatColor.GREEN + "Finding a good place for you, hold on.");
                 runWild((Player) e.getWhoClicked(), plugin.wildWorld1);
                 return;
             }
 
-            if (item.getItemMeta().getDisplayName().equals(ChatColor.RED + "Teleport to a random location in the nether.")){
+            if (item.getItemMeta().getDisplayName().equals(ChatColor.RED + "Teleport to a random location in the nether.")) {
                 player.closeInventory();
-                e.getWhoClicked().sendMessage(ChatColor.GREEN + "Not enabled yet!");
-                //runWild((Player) e.getWhoClicked(), plugin.wildWorld2);
+                e.getWhoClicked().sendMessage(ChatColor.GREEN + "Finding a good place for you, hold on.");
+                runWild((Player) e.getWhoClicked(), plugin.wildWorld2);
                 return;
             }
             e.setCancelled(true);
         }
     }
 
-    private boolean runWild(Player player, String args) {
-        plugin.getServer().getScheduler().runTaskAsynchronously(plugin, new Runnable() {
-            @Override
-            public void run() {
-                World world;
-                if (args != null) {
-                    world = plugin.getServer().getWorld(args);
-                    if (world == null){
-                        world = player.getWorld();
-                    }
-                } else {
+    private void runWild(Player player, String args) {
+        plugin.getServer().getScheduler().runTaskAsynchronously(plugin, () -> {
+            World world;
+            if (args != null) {
+                world = plugin.getServer().getWorld(args);
+                if (world == null) {
                     world = player.getWorld();
                 }
-                String wName = world.getName();
-                Location tp = createTp(player, world);
-                if (tp == null || tp.add(0,2,0).getBlock().getType() != Material.AIR) {
-                    runWild(player, wName);
-                    return;
-                }
-
-                plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
-                    @Override
-                    public void run() {
-                        if (hitNogo) {
-                            runWild(player, wName);
-                        } else {
-                            plugin.cMap().put(player.getUniqueId(), System.currentTimeMillis());
-                            player.sendMessage(ChatColor.GREEN + "Found a location, teleporting!");
-                            player.teleport(tp);
-                            running = false;
-                        }
-                    }
-                }, 20L);
+            } else {
+                world = player.getWorld();
             }
+            String wName = world.getName();
+            Location tp = createTpW(world);
+
+            plugin.getServer().getScheduler().scheduleSyncDelayedTask(plugin, () -> {
+                if (hitNogo) {
+                    runWild(player, wName);
+                } else {
+                    player.sendMessage(ChatColor.GREEN + "Found a location, teleporting!");
+                    if (!(tp == null)){
+                        player.teleport(tp);
+                        plugin.cMap().put(player.getUniqueId(), System.currentTimeMillis());
+                        running = false;
+                    }else{
+                        player.sendMessage(ChatColor.RED + "Couldn't create location... Please report this.");
+                    }
+                }
+            }, 20L);
         });
-        return true;
     }
 
-    private Location createTp(Player player, World world) {
-        int rX = getX();
-        int rZ = getZ();
-        return getHighestBlock(world, rX, rZ);
+    private Location createTpW(World world) {
+        if (world.getName().equalsIgnoreCase(plugin.wildWorld1)) {
+            int rX = w1GetX();
+            int rZ = w1GetZ();
+            return getHighestBlock(world, rX, rZ);
+        }
+        if (world.getName().equalsIgnoreCase(plugin.wildWorld2)) {
+            int rX = w2GetX();
+            int rZ = w2GetZ();
+            return getHighestBlock(world, rX, rZ);
+        }
+        return null;
     }
 
-    private Integer getX() {
-        return (int) (Math.random() * ((maxX - minX) + 1)) + minX;
+    private Integer w1GetX() {
+        return (int) (Math.random() * ((w1MAxX - w1MinX) + 1)) + w1MinX;
     }
 
-    private Integer getZ() {
-        return (int) (Math.random() * ((maxZ - minZ) + 1)) + minZ;
+    private Integer w1GetZ() {
+        return (int) (Math.random() * ((w1MaxZ - w1MinZ) + 1)) + w1MinZ;
+    }
+
+    private Integer w2GetX() {
+        return (int) (Math.random() * ((w2MAxX - w2MinX) + 1)) + w2MinX;
+    }
+
+    private Integer w2GetZ() {
+        return (int) (Math.random() * ((w2MaxZ - w2MinZ) + 1)) + w2MinZ;
     }
 
     private Location getHighestBlock(World world, int x, int z) {
-        /**
-        plugin.getServer().getScheduler().runTask(plugin, new Runnable() {
-            @Override
-            public void run() {
-                plugin.getServer().getWorld(world.getUID()).loadChunk(x, z, true);
-            }
-        });
-         **/
         hitNogo = false;
 
         int i = 255;
@@ -190,12 +190,4 @@ public class Wild implements CommandExecutor, Listener {
         }
         return null;
     }
-
-    /**
-    private boolean SafeBlock(Location tp) {
-        Material block = tp.getBlock().getType();
-        //plugin.getLogger().info(block.toString());
-        return (materials.contains(block));
-    }
-     **/
 }
