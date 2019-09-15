4c4c4d3abc56a20d1b5657c9c03ac616ad0b900e
Stian Tofte <stitofte@outlook.com>
2019-09-16 00:18:50 +0200

    Removed unused import.

--- src/main/java/com/ohneemc/OhneeEssentials/events/PreLogin.java
+++ src/main/java/com/ohneemc/OhneeEssentials/events/PreLogin.java
@@ -2,7 +2,6 @@ package com.ohneemc.OhneeEssentials.events;
 
 import com.ohneemc.OhneeEssentials.OhneeEssentials;
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.AsyncPlayerPreLoginEvent;
