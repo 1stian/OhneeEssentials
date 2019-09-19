e8afabf3220671bc361c20e77800c632e72ac782
Stian Tofte <stitofte@outlook.com>
2019-09-19 02:24:30 +0200

    Will contain code for giving kits to players.

---
+++ src/main/java/com/ohneemc/OhneeEssentials/resources/KitsUtil.java
@@ -0,0 +1,13 @@
+package com.ohneemc.OhneeEssentials.resources;
+
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+
+public class KitsUtil {
+
+    public boolean giveKit(Player player, ArrayList items){
+
+        return false;
+    }
+}
