e28903212d3ec0bbd15fb39a858d49b9c5845cf8
Stian Tofte <stitofte@outlook.com>
2019-09-19 02:25:34 +0200

    Kit command will be here.

---
+++ src/main/java/com/ohneemc/OhneeEssentials/commands/Kit.java
@@ -0,0 +1,12 @@
+package com.ohneemc.OhneeEssentials.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+public class Kit implements CommandExecutor {
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return false;
+    }
+}
